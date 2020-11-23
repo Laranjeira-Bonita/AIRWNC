f54fd2a480d9159a0dd3f7bad33818a85d4041cd
augusto araujo <augusto.za@gmail.com>
2020-11-23 19:21:54 -0300

    models associations and validations (bathrooms, users and bookins) done

---
+++ app/models/booking.rb
@@ -0,0 +1,8 @@
+class Booking < ApplicationRecord
+  belongs_to :client, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :bathroom
+  has_one :owner, through: :bathroom
+  # validates :date, presence: true
+  # validates :duration, presence: true, inclusion: { in: [15, 20, 25, 30, 35, 40, 45] }
+  # validates :bathroom, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :bathrooms
+  has_many :bookings
+  has_many :rented_bathrooms, through: :bookings, source: :bathroom
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
