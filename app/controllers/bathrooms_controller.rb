423938be05279d8d1afb5abcac248f6931417d5a
marcela marotta <marcelamarotta@gmail.com>
2020-11-27 14:48:41 -0300

    map ok with marker

--- app/controllers/bathrooms_controller.rb
+++ app/controllers/bathrooms_controller.rb
@@ -10,7 +10,7 @@ class BathroomsController < ApplicationController
         lat: bathroom.latitude,
         lng: bathroom.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { bathroom: bathroom }),
-        image_url: helpers.asset_url('wc.png')
+        image_url: helpers.asset_url("wc.png")
       }
     end
   end
